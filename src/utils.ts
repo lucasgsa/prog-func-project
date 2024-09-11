2ab65d89c84bd0a7f2ab162b1e5ec5c30e168620
Lucas Almeida <lucasgsalmeida19@gmail.com>
2024-09-11 19:25:26 -0300

    adding utils.ts fold and compose

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,37 @@
-function distinct<T> (collection: T[], attribute: String): T[] {
-    return [];
+// function distinct<T> ([x, ...xs]: T[], attribute: keyof T): T[] {
+//     if (xs.length === 0) {
+//         return [x];
+//     }
+
+//     return xs.includes(x) ? distinct(xs, attribute) : [x, ...distinct(xs, attribute)];
+// }
+
+// function groupBy<T> ([x, ...xs]: T[], attribute: keyof T) : Map<String, T[]> {
+//     const nextValuesGroupBy: Map<String, T[]> = groupBy(xs, attribute);
+//     const chave = x[attribute];
+//     const keyGroup = nextValuesGroupBy[x[attribute]] ?? [];
+
+//     const sameGroupOfX = nextValuesGroupBy[attribute]
+//     const groupKey = x[attribute];
+//     nextValuesGroupBy.keys
+//     return {};
+// }
+
+function fold<T,S>(foldFunc : (current : S, next : T) => S, init : S, [x, ...xs] : T[]) {
+    if (xs.length == 0) {
+        return foldFunc(init, x);
+    }
+
+    return fold(foldFunc, foldFunc(init, x), xs);
 }
 
-function addToMapBucket<T, K> ([x, ...xs]: Map<K, T[]>, key: K, value: T): Map<K, T[]> {
-    if (x === key)
+function compose<Args, R1, R2>(func1: (arg: R2) => R1, func2: (...arg: Args[]) => R2) : (...arg : Args[]) => R1 {
+    return (...args: Args[]) => func1(func2(...args));
 }
 
-function groupBy<T, K extends keyof T> ([x, ...xs]: T[], attribute: K): Map<K, T[]> {
-    const nextValuesGroupBy: Map<K, T[]> = groupBy(xs, attribute);
-    const groupKey = x[attribute];
-    nextValuesGroupBy.keys
+const main = () => {
+    const componseFunc = compose(console.log, (...numbers : number[]) => fold((a : number, b : number) => a+b, 0, numbers));
+    componseFunc(1, 2, 3, 4, 5);
 }
+
+main();
