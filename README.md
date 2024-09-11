0b9a91423d7b4a7963fce9e5f60b7291acc06f65
Lucas Almeida <lucasgsalmeida19@gmail.com>
2024-09-11 20:17:53 -0300

    creating distinct and groupBy functions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # prog-func-project
+
+Setup:
+
+Instalar dependências:
+`yarn install`
+
+Rodar um arquivo sem necessidade de compilar:
+`yarn start src/utils.ts`
--- src/utils.ts
+++ src/utils.ts
@@ -1,37 +1,27 @@
-// function distinct<T> ([x, ...xs]: T[], attribute: keyof T): T[] {
-//     if (xs.length === 0) {
-//         return [x];
-//     }
+function distinct<T> (list: T[], attribute: keyof T): T[] {
+    return Object.values(groupBy(list, attribute))
+            .map(entry => entry[0]);
+}
 
-//     return xs.includes(x) ? distinct(xs, attribute) : [x, ...distinct(xs, attribute)];
-// }
+function groupBy<T> (list: T[], attribute: keyof T) : { [key: string]: T[] } {
+    if (list.length === 0) return {};
 
-// function groupBy<T> ([x, ...xs]: T[], attribute: keyof T) : Map<String, T[]> {
-//     const nextValuesGroupBy: Map<String, T[]> = groupBy(xs, attribute);
-//     const chave = x[attribute];
-//     const keyGroup = nextValuesGroupBy[x[attribute]] ?? [];
+    const [x, ...xs] = list;
+    const nextValuesGroupBy = groupBy(xs, attribute);
 
-//     const sameGroupOfX = nextValuesGroupBy[attribute]
-//     const groupKey = x[attribute];
-//     nextValuesGroupBy.keys
-//     return {};
-// }
+    const key = String(x[attribute]);
+    const group = nextValuesGroupBy[key] ?? [];
+
+    return { ...nextValuesGroupBy, [key]: [...group, x] };
+}
 
 function fold<T,S>(foldFunc : (current : S, next : T) => S, init : S, [x, ...xs] : T[]) {
-    if (xs.length == 0) {
-        return foldFunc(init, x);
-    }
-
+    if (xs.length == 0) return foldFunc(init, x);
     return fold(foldFunc, foldFunc(init, x), xs);
 }
 
-function compose<Args, R1, R2>(func1: (arg: R2) => R1, func2: (...arg: Args[]) => R2) : (...arg : Args[]) => R1 {
-    return (...args: Args[]) => func1(func2(...args));
+function compose<Args extends unknown[], R1, R2>(func1: (arg: R2) => R1, func2: (...arg: Args) => R2) : (...arg : Args) => R1 {
+    return (...args: Args) => func1(func2(...args));
 }
 
-const main = () => {
-    const componseFunc = compose(console.log, (...numbers : number[]) => fold((a : number, b : number) => a+b, 0, numbers));
-    componseFunc(1, 2, 3, 4, 5);
-}
-
-main();
+console.log('oi');
