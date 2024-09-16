4714eb3f0eb843f07b1759eeab83a8c3b5808f60
119110385 LUCAS GABRIEL SOARES DE ALMEIDA <lucas.gabriel.almeida@lcc2-40.lcc.ufcg.edu.br>
2024-09-16 17:16:52 -0300

    creating tests to distinct and groupBy

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
-function distinct<T> (list: T[], attribute: keyof T): T[] {
+export function distinct<T> (list: T[], attribute: keyof T): T[] {
     return Object.values(groupBy(list, attribute))
             .map(entry => entry[0]);
 }
 
-function groupBy<T> (list: T[], attribute: keyof T) : { [key: string]: T[] } {
+export function groupBy<T> (list: T[], attribute: keyof T) : { [key: string]: T[] } {
     if (list.length === 0) return {};
 
     const [x, ...xs] = list;
@@ -15,13 +15,11 @@ function groupBy<T> (list: T[], attribute: keyof T) : { [key: string]: T[] } {
     return { ...nextValuesGroupBy, [key]: [...group, x] };
 }
 
-function fold<T,S>(foldFunc : (current : S, next : T) => S, init : S, [x, ...xs] : T[]) {
+export function fold<T,S>(foldFunc : (current : S, next : T) => S, init : S, [x, ...xs] : T[]) {
     if (xs.length == 0) return foldFunc(init, x);
     return fold(foldFunc, foldFunc(init, x), xs);
 }
 
-function compose<Args extends unknown[], R1, R2>(func1: (arg: R2) => R1, func2: (...arg: Args) => R2) : (...arg : Args) => R1 {
+export function compose<Args extends unknown[], R1, R2>(func1: (arg: R2) => R1, func2: (...arg: Args) => R2) : (...arg : Args) => R1 {
     return (...args: Args) => func1(func2(...args));
 }
-
-exports.distinct = distinct;
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,32 +1,76 @@
-import { distinct } from '../src/utils';
+import { distinct, groupBy } from '../src/utils';
 
 describe('Testing utils functions', () => {
 
     describe('distinct', () => {
-        test('case test 1', () => {
+        test('distinct with multiple values', () => {
             const data = [
-                {
-                    "name": "Alfredo",
-                    "id": 1
-                },
-                {
-                    "name": "José",
-                    "id": 2
-                },
-                {
-                    "name": "Alfredo",
-                    "id": 3
-                },
-                {
-                    "name": "Idk",
-                    "id": 4
-                }
+                { "name": "Alfredo", "id": 1 },
+                { "name": "José", "id": 2 },
+                { "name": "Alfredo", "id": 3 },
+                { "name": "Idk", "id": 4 }
             ];
 
-            const distinctResult = distinct(data, 'name');
-            console.log(distinctResult);
+            const result = distinct(data, 'name');
+
+            expect(result).toEqual([
+                { name: 'Idk', id: 4 },
+                { name: 'Alfredo', id: 3 },
+                { name: 'José', id: 2 }
+              ]);
         });
 
+        test('distinct with empty list', () => {
+            const data: any[] = [];
+            const result = distinct(data, 'name');
+            expect(result).toEqual([]);
+        });
+
+        test('distinct with list with just one value', () => {
+            const data = ["1"];
+            const result = distinct(data, 'toString');
+            expect(result).toEqual(["1"]);
+        });
+
+    });
+
+    describe('groupBy', () => {
+        test('groupBy with multiple values', () => {
+            const data = [
+                { "name": "Alfredo", "id": 1 },
+                { "name": "José", "id": 2 },
+                { "name": "Alfredo", "id": 3 },
+                { "name": "Idk", "id": 4 }
+            ];
+
+            const result = groupBy(data, 'name');
+
+            expect(result).toEqual({
+                "Alfredo": [{ "name": "Alfredo", "id": 1 }, { "name": "Alfredo", "id": 3 }],
+                "Idk" : [{ name: "Idk", id: 4 }],
+                "José": [{ name: "José", id: 2 }]
+            });
+        });
+
+        test('groupBy with empty list', () => {
+            const data: any[] = [];
+            const result = groupBy(data, 'name');
+            expect(result).toEqual({});
+        });
+
+        test('groupBy with list with just one value', () => {
+            const data = [{ id: 1 }];
+            const result = groupBy(data, 'id');
+            expect(result).toEqual({ 1: [ { id: 1} ] });
+        });
+    });
+
+    describe('fold', () => {
+        // TODO
+    });
+
+    describe('compose', () => {
+        // TODO
     });
 
   });
